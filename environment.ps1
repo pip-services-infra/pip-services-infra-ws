cb2f93418e48df94055fc430a11fede5c4bb54e1
Sergey Seroukhov <seroukhov@gmail.com>
2017-03-09 22:05:48 -0700

    Initial commit

---
+++ environment.ps1
@@ -0,0 +1,11 @@
+$global:Services = @(
+    "pip-services-logging-node"
+    "pip-services-syslog-node"
+    "pip-services-counters-node"
+)
+
+$global:Clients = @(
+    "pip-clients-logging-node"
+    "pip-clients-syslog-node"
+    "pip-clients-counters-node"
+)
---
+++ workspace.conf.ps1
@@ -0,0 +1,11 @@
+$VersionControl = 'git'
+$VersionControlRepos = @(
+	"https://github.com/pip-services/pip-services-devenv.git"
+
+	"https://github.com/pip-services-infra/pip-services-logging-node.git"
+
+	"https://github.com/pip-services-infra/pip-services-syslog-node.git"
+	"https://github.com/pip-services-infra/pip-clients-syslog-node.git"
+
+	"https://github.com/pip-services-infra/pip-services-counters-node.git"
+)
