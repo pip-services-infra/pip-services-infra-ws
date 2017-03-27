6682b21105276813143d7e63cce9cacbd8ef46f6
Sergey Seroukhov <seroukhov@gmail.com>
2017-03-26 22:27:55 -0700

    Updated path to repository

--- workspace.conf.ps1
+++ workspace.conf.ps1
@@ -2,10 +2,10 @@ $VersionControl = 'git'
 $VersionControlRepos = @(
 	"https://github.com/pip-services/pip-services-devenv.git"
 
-	"https://github.com/pip-services-infra/pip-services-logging-node.git"
+	"https://github.com/pip-services-infrastructure/pip-services-logging-node.git"
 
-	"https://github.com/pip-services-infra/pip-services-syslog-node.git"
-	"https://github.com/pip-services-infra/pip-clients-syslog-node.git"
+	"https://github.com/pip-services-infrastructure/pip-services-syslog-node.git"
+	"https://github.com/pip-services-infrastructure/pip-clients-syslog-node.git"
 
-	"https://github.com/pip-services-infra/pip-services-counters-node.git"
+	"https://github.com/pip-services-infrastructure/pip-services-counters-node.git"
 )
