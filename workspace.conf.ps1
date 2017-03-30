ef426be5009e1f19061d5743f855aa575dcd3ddb
Sergey Seroukhov <seroukhov@gmail.com>
2017-03-30 15:52:51 -0700

    Added client repos for logging and counters

--- workspace.conf.ps1
+++ workspace.conf.ps1
@@ -3,9 +3,11 @@ $VersionControlRepos = @(
 	"https://github.com/pip-services/pip-services-devenv.git"
 
 	"https://github.com/pip-services-infrastructure/pip-services-logging-node.git"
+	"https://github.com/pip-services-infrastructure/pip-clients-logging-node.git"
 
 	"https://github.com/pip-services-infrastructure/pip-services-eventlog-node.git"
 	"https://github.com/pip-services-infrastructure/pip-clients-eventlog-node.git"
 
 	"https://github.com/pip-services-infrastructure/pip-services-counters-node.git"
+	"https://github.com/pip-services-infrastructure/pip-clients-counters-node.git"
 )
